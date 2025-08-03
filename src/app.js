84d324f1afcdbd4baacdcf77f83f36d56fd38cbd
Shivakumar Kirigeri <shivakumar641@gmail.com>
2025-08-03 23:10:23 +0530

    task:auth done. logout i progress

--- src/app.js
+++ src/app.js
@@ -3,8 +3,9 @@ const app = new express();
 const connectDatabase = require("./database/dbManager");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const dummyRouter = require("./routers/dummyRouter");
+//const dummyRouter = require("./routers/dummyRouter");
 const serviceRouter = require("./routers/serviceRouter");
+const authRouter = require("./routers/authRouter");
 app.use(express.json());
 app.use(cookieParser());
 app.use(
@@ -13,8 +14,9 @@ app.use(
     credentials: true,
   })
 );
-app.use("/", dummyRouter);
+//app.use("/", dummyRouter);
 app.use("/", serviceRouter);
+app.use("/", authRouter);
 connectDatabase()
   .then(() => {
     console.log("Database connected successfully.");
--- src/routers/serviceRouter.js
+++ src/routers/serviceRouter.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const serviceData = require("../models/serviceData");
 const serviceRouter = express.Router();
-
-serviceRouter.get("/getservicedvehicles", async (req, res) => {
-  const garageid = "688e537a254c37b0e3d3283c";
-  const result = await serviceData.find({
-    $and: [
-      { fkgarageId: garageid },
-      { fkvehicleDataId: "688e6047cd7c61f602d1d28e" },
-    ],
-  });
-  res.json({ status: "Ok", data: result });
-});
+const checkAuthentication = require("./middleware/checkAuthentication");
+serviceRouter.get(
+  "/getservicedvehicles",
+  checkAuthentication,
+  async (req, res) => {
+    console.log(req.credentialsData);
+    const result = await serviceData.find({
+      $and: [
+        { fkgarageId: req.credentialsData._id },
+        { fkvehicleDataId: "688e6047cd7c61f602d1d28e" },
+      ],
+    });
+    res.json({ status: "Ok", data: result });
+  }
+);
 module.exports = serviceRouter;
