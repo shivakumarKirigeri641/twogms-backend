d089c01c3d89001aea529f09494bf9aa29820c03
Shivakumar Kirigeri <shivakumar641@gmail.com>
2025-08-05 12:04:34 +0530

    task:recollecting points from branch 1

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -4,37 +4,42 @@ const createJWTToken = require("../utils/createJWTToken");
 const authRouter = express.Router();
 const garageData = require("../models/garageData");
 //register garage (do it later as it includes garage entry, stdservice entry, wash, lbr details)
+//to register, garage owner needs to provide
+//1. std service price, list of services. (either individual prcies or all in one std service price) (any photo)
+//2. labour charges
+//3. washing charges
+//4. air-blow charges (if applicable)
+//5. pickup-drop charges (if applicable)
+//6. no. of staffs & their name & phone numbers
+//7. We provide with owner login credentials & staff credentials
 
 //login
 authRouter.post("/twogms/login", async (req, res) => {
   let garagedetails = null;
   try {
-    const { phoneNumber, password } = req.body;
-    if (phoneNumber && password) {
-      garagedetails = await garageData.findOne({
-        garageOwnerMobileNumber: phoneNumber,
-      });
-      if (!garagedetails) {
-        throw new Error("Invalid credentials!");
-      }
-      const ispwdcorrect = await argon2.verify(
-        garagedetails.password,
-        password
-      );
-
-      if (!ispwdcorrect) {
-        throw new Error("Invalid credentials!");
-      }
-      req.credentialsData = garagedetails;
-      const token = await createJWTToken(req);
-      res.cookie("token", token);
-    } else {
-      throw new Error("Invalid credentials!");
-    }
     res.status(200).json({ status: "Ok", data: garagedetails });
   } catch (err) {
     res.json({ status: "Failed", message: err.message });
   }
 });
 //logout
+authRouter.post("/twogms/logout", checkAuthentication, async (req, res) => {
+  try {
+    res.status(200).json({ status: "Ok", message: "Logout successful" });
+  } catch (err) {
+    res.json({ status: "Failed", message: err.message });
+  }
+});
+//register
+authRouter.post("/twogms/gargeregistration", async (req, res) => {
+  try {
+    res.status(200).json({
+      status: "Ok",
+      message: "Garage registration successful",
+      data: null,
+    });
+  } catch (err) {
+    res.json({ status: "Failed", message: err.message });
+  }
+});
 module.exports = authRouter;
