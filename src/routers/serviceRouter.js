997ad1efdde30ebca03264218109d75e383c6841
Shivakumar Kirigeri <shivakumar641@gmail.com>
2025-08-23 22:51:03 +0530

    task: edit servicing vehicle api (get) done

--- src/routers/serviceRouter.js
+++ src/routers/serviceRouter.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const serviceData = require("../models/serviceData");
 const checkAuthentication = require("../middleware/checkAuthentication");
+const { populate } = require("dotenv");
 require("dotenv").config();
 const serviceRouter = express.Router();
 
@@ -73,6 +74,75 @@ serviceRouter.get(
         })
         .select("serviceSequenceNumber kmDriven vehicleInDate")
         .sort({ vehicleInDate: -1 });
+      res.status(200).json({ status: "Ok", data: result });
+    } catch (err) {
+      res.status(401).json({ status: "Failed", message: err.message });
+    }
+  }
+);
+
+//edit-servicing vehicle
+serviceRouter.get(
+  "/twogms/servicing-vehicle/:serviceid",
+  checkAuthentication,
+  async (req, res) => {
+    try {
+      const result = await serviceData
+        .findById(req.params.serviceid)
+        .populate({
+          path: "fkVehicleDataId",
+          populate: {
+            path: "fkTwoWheelerDataId",
+          },
+        })
+        .populate({
+          path: "fkVehicleDataId",
+          populate: {
+            path: "vehicleNumber",
+          },
+        })
+        .populate({
+          path: "fkVehicleDataId",
+          populate: {
+            path: "fkCustomerDataId",
+          },
+        })
+        .populate({
+          path: "fkGarageDataId",
+        })
+        .populate({
+          path: "fkAfterServiceComplaintsDataId",
+        })
+        .populate({
+          path: "fkCustomerComplaintsDataId",
+        })
+        .populate({
+          path: "fkAssignedStaffsDataId",
+          populate: "staffs.fkStaffDataId",
+        })
+        .populate({
+          path: "fkMechanicObservationsDataId",
+        })
+        .populate({
+          path: "fkPartsAndAccessoriesDataId",
+        })
+        .populate({
+          path: "fkServiceDeliveryDataId",
+        })
+        .populate({
+          path: "fkServiceFeedbackDataId",
+        })
+        .populate({
+          path: "fkNextServiceDataId",
+        })
+        .populate({
+          path: "fkBillingDataId",
+          populate:
+            "labourServiceChargesData.fkLabourServiceChargesData washingServiceChargesData.fkWashingServiceChargesData pickupServiceChargesData.fkPickupServiceChargesData dropServiceChargesData.fkDropServiceChargesData partsAndAccessoriesData.fkPartsAndAccessoriesDataId",
+        });
+      if (!result) {
+        throw new Error("Service information not found!");
+      }
       res.status(200).json({ status: "Ok", data: result });
     } catch (err) {
       res.status(401).json({ status: "Failed", message: err.message });
