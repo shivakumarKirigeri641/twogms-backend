d470d3f52961c5cfe8ac0f5eb583f5981da41ae5
Shivakumar Kirigeri <shivakumar641@gmail.com>
2025-08-27 22:25:13 +0530

    task:get details for edit-serviced-vehicle (change in api)

--- src/routers/twoWheelerRouter.js
+++ src/routers/twoWheelerRouter.js
@@ -41,4 +41,88 @@ twoWheelerRouter.get(
     }
   }
 );
+
+//repeat-vehicle service
+twoWheelerRouter.get(
+  "/twogms/edit-serviced-vehicle/:vehiclenumber",
+  checkAuthentication,
+  async (req, res) => {
+    try {
+      const result = await vehicleData
+        .findOne({
+          vehicleNumber: req.params.vehiclenumber.toUpperCase(),
+        })
+        .populate("fkCustomerDataId fkTwoWheelerDataId");
+      if (!result) {
+        throw new Error(
+          `Vehicle number:${req.params.vehiclenumber} not found!`
+        );
+      }
+      const serviceinfo = await serviceData
+        .find({
+          $and: [
+            {
+              fkGarageDataId: req?.loginCredentials?.fkGarageDataId,
+              fkVehicleDataId: result?._id,
+            },
+          ],
+        })
+        .populate({
+          path: "fkVehicleDataId",
+          populate: {
+            path: "fkTwoWheelerDataId",
+          },
+        })
+        .populate({
+          path: "fkVehicleDataId",
+          populate: {
+            path: "vehicleNumber",
+          },
+        })
+        .populate({
+          path: "fkVehicleDataId",
+          populate: {
+            path: "fkCustomerDataId",
+          },
+        })
+        .populate({
+          path: "fkGarageDataId",
+        })
+        .populate({
+          path: "fkAfterServiceComplaintsDataId",
+        })
+        .populate({
+          path: "fkCustomerComplaintsDataId",
+        })
+        .populate({
+          path: "fkAssignedStaffsDataId",
+          populate: "staffs.fkStaffDataId",
+        })
+        .populate({
+          path: "fkMechanicObservationsDataId",
+        })
+        .populate({
+          path: "fkPartsAndAccessoriesDataId",
+        })
+        .populate({
+          path: "fkServiceDeliveryDataId",
+        })
+        .populate({
+          path: "fkServiceFeedbackDataId",
+        })
+        .populate({
+          path: "fkNextServiceDataId",
+        })
+        .populate({
+          path: "fkBillingDataId",
+          populate:
+            "labourServiceChargesData.fkLabourServiceChargesData washingServiceChargesData.fkWashingServiceChargesData pickupServiceChargesData.fkPickupServiceChargesData dropServiceChargesData.fkDropServiceChargesData partsAndAccessoriesData.fkPartsAndAccessoriesDataId",
+        })
+        .sort({ serviceSequenceNumber: -1 });
+      res.status(200).json({ status: "Ok", data: { result, serviceinfo } });
+    } catch (err) {
+      res.status(401).json({ status: "Failed", message: err.message });
+    }
+  }
+);
 module.exports = twoWheelerRouter;
